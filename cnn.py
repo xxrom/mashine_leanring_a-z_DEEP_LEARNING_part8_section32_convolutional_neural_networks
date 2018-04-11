ce4b662940e5b934de06de519a5799ec2193caa3
Nikita Chernyshov <xpom55@mail.ru>
2018-04-11 17:09:13 +0300

    l247 importing packges for CNN

--- cnn.py
+++ cnn.py
@@ -9,4 +9,13 @@
 # Installing Keras
 # pip install --upgrade keras
 
+# здесь не будет Data Preprocessing как в ANN, так как данные уже разбиты
+# на категории для обучения и для тестирования нейронки
+# поэтому сразу приступаем разрабатывать CNN
 # Part 1 - Building the CNN
+from keras.models import Sequential # for init NN для слоев нейронки
+from keras.layers import Convolution2D # для первого слоя CNN (2D - imges)
+from keras.layers import MaxPooling2D # for pooling layers (2 step)
+from keras.layers import Flatten # for flattening (3 step)
+from keras.layers import Dense # add fully connected layer in classic ANN
+
